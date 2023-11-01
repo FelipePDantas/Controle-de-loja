05cce11c57aeef7c43520c12151b76a8f81549cc
Felipe Pereira <felipejiti@hotmail.com>
2023-11-01 14:59:14 -0300

    Projeto criado

---
+++ src/main/java/com/ControleDePedidos/api/ControleDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.ControleDePedidos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ControleDePedidos/api/ControleDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ControleDePedidos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
