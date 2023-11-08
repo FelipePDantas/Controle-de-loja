725df0d599b2b8819f54638eb815caeb7130e9eb
Felipe Pereira <felipejiti@hotmail.com>
2023-11-08 17:09:04 -0300

    Adicionado a Associação de muitos para muitos na entidade Category

--- src/main/java/com/ControleDePedidos/api/entities/Category.java
+++ src/main/java/com/ControleDePedidos/api/entities/Category.java
@@ -1,5 +1,6 @@
 package com.ControleDePedidos.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,8 @@ public class Category implements Serializable {
 
     private String nome;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){}
