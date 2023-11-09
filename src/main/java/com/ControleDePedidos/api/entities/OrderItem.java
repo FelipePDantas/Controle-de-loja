c34a27395fe661de56280eb909e5f3d4f964a999
Felipe Pereira <felipejiti@hotmail.com>
2023-11-09 11:09:57 -0300

    Adicionado a quantidade e preço com junção com o pedido e produto

---
+++ src/main/java/com/ControleDePedidos/api/entities/OrderItem.java
@@ -0,0 +1,59 @@
+package com.ControleDePedidos.api.entities;
+
+import com.ControleDePedidos.api.entities.pk.OrdemItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_Order_item")
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrdemItemPK id;
+
+    private Integer quantidade;
+
+    private Double preco;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order, Product product, Integer quantidade, Double preco) {
+        this.quantidade = quantidade;
+        this.preco = preco;
+        id.setOrder(order);
+        id.setProduct(product);
+    }
+    public Order getOrder(){
+        return id.getOrder();
+    }
+    public void setOrder(Order order){
+       id.setOrder(order);
+    }
+    public Product getProduct(){
+        return id.getProduct();
+    }
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
