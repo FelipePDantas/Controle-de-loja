c2dfd5624fda441de2af57e3ba994db2db674f3c
Felipe Pereira <felipejiti@hotmail.com>
2023-11-09 11:12:07 -0300

    Inclusão de uma entidade para adicionar a order e o produto

---
+++ src/main/java/com/ControleDePedidos/api/entities/pk/OrdemItemPK.java
@@ -0,0 +1,52 @@
+package com.ControleDePedidos.api.entities.pk;
+
+import com.ControleDePedidos.api.entities.Order;
+import com.ControleDePedidos.api.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrdemItemPK implements Serializable  {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrdemItemPK that = (OrdemItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
